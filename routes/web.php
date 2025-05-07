cb7e5861e79daf364af6d89485acc3077d7564ed
hoangnm2k1 <hoangnm2k1@gmail.com>
2025-05-07 23:04:25 +0700

    Jobs Page: Tag Component & Job Info

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('', fn() => to_route('jobs.index'));
 Route::resource('jobs', JobController::class)
     ->only(['index']);
