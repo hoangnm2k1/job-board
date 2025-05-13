47d063ea8b336d8acd0f6b52aaddace1c5e6dd9d
hoangnm2k1 <hoangnm2k1@gmail.com>
2025-05-13 11:44:26 +0700

    Filtering Jobs: Form & Searching for Text in Job Posts

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -12,7 +12,14 @@ class JobController extends Controller
      */
     public function index()
     {
-        return view('components.job.index', ['jobs' => Job::all()]);
+        $jobs = Job::query();
+
+        $jobs->when(request('search'), function($query) {
+            $query->where('title', 'like', '%' . request('search') .'%')
+                ->orWhere('description', 'like', '%' . request('search') .'%');
+        });
+
+        return view('components.job.index', ['jobs' => $jobs->get()]);
     }
 
     /**
