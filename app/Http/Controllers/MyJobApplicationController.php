6b26604fdd0485b308ff7a0dc5bde6b7b223fb98
hoangnm2k1 <hoangnm2k1@gmail.com>
2025-06-02 23:14:39 +0700

    My Applications: Cancelling Applications

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobApplication;
 use Illuminate\Http\Request;
 
 class MyJobApplicationController extends Controller
@@ -18,57 +19,16 @@ class MyJobApplicationController extends Controller
                 ->with([
                     'job' => fn($query) => $query->withCount('jobApplications')
                         ->withAvg('jobApplications', 'expected_salary'),
-                        'job.employer'
+                    'job.employer'
         ])
                 ->latest()->get()
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function destroy(JobApplication $myJobApplication)
     {
-        //
-    }
+        $myJobApplication->delete();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return redirect()->back()->with('success', 'Job application removed');
     }
 }
