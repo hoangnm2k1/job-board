eda5c155487247eeb29323c057063f61637af83c
hoangnm2k1 <hoangnm2k1@gmail.com>
2025-06-18 22:49:37 +0700

    Employer: Job Policy

--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -13,6 +13,7 @@ class MyJobController extends Controller
      */
     public function index()
     {
+        $this->authorize('viewAnyEmployer', Job::class);
         return view('my_job.index',
         [
             'jobs' => auth()->user()->employer
@@ -28,6 +29,7 @@ class MyJobController extends Controller
      */
     public function create()
     {
+        $this->authorize('create', Job::class);
         return view('my_job.create');
     }
 
@@ -36,6 +38,7 @@ class MyJobController extends Controller
      */
     public function store(JobRequest $request)
     {
+        $this->authorize('create', Job::class);
         auth()->user()->employer->jobs()->create($request ->validated());
 
         return redirect()->route('my-jobs.index')
@@ -55,6 +58,7 @@ class MyJobController extends Controller
      */
     public function edit(Job $myJob)
     {
+        $this->authorize('update', $myJob);
         return view('my_job.edit', ['job' => $myJob]);
     }
 
@@ -63,6 +67,7 @@ class MyJobController extends Controller
      */
     public function update(JobRequest $request, Job $myJob)
     {
+        $this->authorize('update', $myJob);
         $myJob->update($request->validated());
 
         return redirect()->route('my-jobs.index')
